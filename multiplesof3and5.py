2d52a754c87224c97dc26b5740c611fa76e3fb55
papacheco <pedro.pacheco.a@gmail.com>
2017-06-12 17:14:20 -0300

    Completo

--- multiplesof3and5.py
+++ multiplesof3and5.py
@@ -10,9 +10,15 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 Only numbers 3 and 5.
 
 """
-import math
 
 def multiple(pos):
+    lista = range(pos)
+    portres = lista[::3]
+    porcinco = lista[::5]
+    somatres = sum(portres)
+    somacinco = sum(porcinco)
+    return somatres + somacinco
+"""
     say = str(pos)
 
     if pos % 5 == 0 or pos % 3 == 0:
@@ -20,7 +26,8 @@ def multiple(pos):
 
     #Numeros que nao se encaixam acima
     return say
-
+"""
+"""
 #Testes
 if __name__ == '__main__':
 
@@ -37,14 +44,37 @@ if __name__ == '__main__':
     assert multiple(15) == 'mult 3 or 5'
 
 
-
+"""
 
 
 """
+In [2]: num = range(10)
 In [35]: sum(range(10))
 Out[35]: 45
 
 #Falta ver como pegar cada numero que se enquadre nos parametros de 3 e 5
         #soma = sum(range(pos))
+        
+-----------------------------------------------------------------------
+In [3]: num
+Out[3]: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+In [4]: for i in num:
+   ...:     if i % 3 == 0:
+   ...:         print(i)
+-----------------------------------------------------------------------
+RESULTADO MUUUITO SIMPLES
+
+In [10]: volt = range(10)
+
+In [11]: volt[::3]
+Out[11]: [0, 3, 6, 9]
+
+In [12]: volt[::5]
+Out[12]: [0, 5]
+
+In [13]: volt[::5]
+
+Total = 266333
 
 """
