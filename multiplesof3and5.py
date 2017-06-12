8accdd11e92420a2bf5ea6a54f7499de2a2f30c2
papacheco <pedro.pacheco.a@gmail.com>
2017-06-12 14:31:29 -0300

    Semi-completo

--- multiplesof3and5.py
+++ multiplesof3and5.py
@@ -10,18 +10,16 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 Only numbers 3 and 5.
 
 """
+import math
 
 def multiple(pos):
-    #Verifica multiplos de 5
-    if pos in (5, 10, 15):
-        return 'mult 5'
+    say = str(pos)
 
-    # Verifica multiplos de 3
-    if pos in (3, 6, 9):
-        return 'mult 3'
+    if pos % 5 == 0 or pos % 3 == 0:
+        say = 'mult 3 or 5'
 
-    #Numeros que nao se encaixam ascima
-    return str(pos)
+    #Numeros que nao se encaixam acima
+    return say
 
 #Testes
 if __name__ == '__main__':
@@ -30,10 +28,23 @@ if __name__ == '__main__':
     assert multiple(2) == '2'
     assert multiple(4) == '4'
 
-    assert multiple(3) == 'mult 3'
-    assert multiple(6) == 'mult 3'
-    assert multiple(9) == 'mult 3'
+    assert multiple(3) == 'mult 3 or 5'
+    assert multiple(6) == 'mult 3 or 5'
+    assert multiple(9) == 'mult 3 or 5'
 
-    assert multiple(5) == 'mult 5'
-    assert multiple(10) == 'mult 5'
-    assert multiple(15) == 'mult 5'
+    assert multiple(5) == 'mult 3 or 5'
+    assert multiple(10) == 'mult 3 or 5'
+    assert multiple(15) == 'mult 3 or 5'
+
+
+
+
+
+"""
+In [35]: sum(range(10))
+Out[35]: 45
+
+#Falta ver como pegar cada numero que se enquadre nos parametros de 3 e 5
+        #soma = sum(range(pos))
+
+"""
